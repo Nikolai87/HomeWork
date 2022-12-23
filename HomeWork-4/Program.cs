8084527ec53d206d12df4510f5dd14b7fda281e0
Nikolai <luzinnikolai@mail.ru>
2022-12-23 20:38:43 +0500

    задача 8

---
+++ HomeWork-4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 о N.
+
+Console.WriteLine ("Введите число ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= N; i += 2)
+    {
+        if (i == N)
+        {
+            Console.WriteLine($"{i}");
+        }
+        else Console.WriteLine($"{i}");
+    }
